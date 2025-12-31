8146b1137575644e49a11f5a3fea981eec0ed90f
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-12-31 18:18:07 -0500

    feat: basic DMA setup with continuous ADC sampling. currently using a loopback analog signal which takes the 3.3V output and makes a voltage divider with a potentiometer for the input signal.

--- src/main.c
+++ src/main.c
@@ -1,47 +1,113 @@
-/* USER CODE BEGIN Header */
-/**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2025 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
-
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+
+#include "defs.h"
+#include "main.h"
 
 #include "stm32f4xx_hal.h"
 
 UART_HandleTypeDef huart2;
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+TIM_HandleTypeDef htim3;
+
+#define SZ 1024
+#define VOLTAGE_MAX 3.3
+#define ADC_MAX 4095.0
+
+#define LED_PIN GPIO_PIN_5
+
+typedef struct {
+    _Bool active_buf : 1;
+    _Bool dma_complete : 1;
+} Flags;
+
+volatile Flags FLAGS;
+volatile u16 ADC_SAMPLES[2 * SZ];
 
 static void sysclock_init(void);
 static void gpio_init(void);
 static void uart2_init(void);
-int _write(int file, char *ptr, int len);
+static void adc_init(void);
+static void dma_init(void);
 static void handle_error(void);
+
+int _write(int file, char *ptr, int len);
+
+double adc_to_voltage(u16 val) { return VOLTAGE_MAX * val / ADC_MAX; }
+
+static void toggle_led() { HAL_GPIO_TogglePin(LD2_GPIO_Port, LED_PIN); }
 
 int main(void) {
     HAL_Init();
     sysclock_init();
     gpio_init();
     uart2_init();
+    dma_init();
+    adc_init();
     setbuf(stdout, NULL);
+    printf("starting!\n");
+    HAL_ADC_Start_DMA(&hadc1, (u32 *)ADC_SAMPLES, 2 * SZ);
 
     while (1) {
-        printf("STM32 printing example\n");
-        HAL_Delay(1000);
+        toggle_led();
+        if (FLAGS.dma_complete) {
+            u64 sum = 0;
+            u16 *buf = ADC_SAMPLES + FLAGS.active_buf * SZ;
+            for (usize i = 0; i < SZ; ++i) {
+                sum += buf[i];
+            }
+            u16 avg = sum / SZ;
+            double avg_voltage = adc_to_voltage(avg);
+            printf("avg voltage: %lfV\n", avg_voltage);
+        } else {
+            printf("not yet\n");
+        }
+        HAL_Delay(500);
+    }
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    if (hadc->Instance == ADC1) {
+        __HAL_RCC_ADC1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        GPIO_InitStruct.Pin = GPIO_PIN_0;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    }
+}
+
+static void adc_init() {
+    ADC_ChannelConfTypeDef sConfig = {0};
+
+    hadc1.Instance = ADC1;
+    hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+    hadc1.Init.ScanConvMode = DISABLE;
+    hadc1.Init.ContinuousConvMode = ENABLE;
+    hadc1.Init.DiscontinuousConvMode = DISABLE;
+    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.NbrOfConversion = 1;
+    hadc1.Init.DMAContinuousRequests = ENABLE;
+    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    if (HAL_ADC_Init(&hadc1) != HAL_OK) {
+        printf("error initializing ADC\n");
+        handle_error();
+    }
+
+    /** Configure for the selected ADC regular channel its corresponding rank in
+     * the sequencer and its sample time. */
+    sConfig.Channel = ADC_CHANNEL_0;
+    sConfig.Rank = 1;
+    sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK) {
+        printf("error initializing ADC\n");
+        handle_error();
     }
 }
 
@@ -49,12 +115,9 @@ static void sysclock_init(void) {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /* Configure the main internal regulator output voltage */
     __HAL_RCC_PWR_CLK_ENABLE();
     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure. */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -66,18 +129,20 @@ static void sysclock_init(void) {
     RCC_OscInitStruct.PLL.PLLQ = 2;
     RCC_OscInitStruct.PLL.PLLR = 2;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        printf("error initializing sysclock\n");
         handle_error();
     }
 
     /** Initializes the CPU, AHB and APB buses clocks */
-    RCC_ClkInitStruct.ClockType =
-        RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
     if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        printf("error initializing sysclock\n");
         handle_error();
     }
 }
@@ -125,9 +190,46 @@ int _write(int file, char *ptr, int len) {
     return len;
 }
 
+static void dma_init(void) {
+    /* DMA controller clock enable */
+    __HAL_RCC_DMA2_CLK_ENABLE();
+
+    /* Configure DMA for ADC1 */
+    hdma_adc1.Instance = DMA2_Stream0;
+    hdma_adc1.Init.Channel = DMA_CHANNEL_0;
+    hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
+    hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
+    hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+    hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+    hdma_adc1.Init.Mode = DMA_CIRCULAR;
+    hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
+    hdma_adc1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+    HAL_DMA_Init(&hdma_adc1);
+
+    /* Associate the DMA handle with the ADC handle */
+    __HAL_LINKDMA(&hadc1, DMA_Handle, hdma_adc1);
+
+    /* DMA interrupt init */
+    /* DMA2_Stream0_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+    if (hadc->Instance == ADC1) {
+        FLAGS.dma_complete = 1;
+    } else {
+        handle_error();
+    }
+}
+
+void DMA2_Stream0_IRQHandler(void) { HAL_DMA_IRQHandler(&hdma_adc1); }
+
 static void handle_error(void) {
-    printf("handle_error()\n");
     __disable_irq();
     while (1) {
+        toggle_led();
+        HAL_Delay(500);
     }
 }
